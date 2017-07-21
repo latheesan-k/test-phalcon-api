5c81ff99b861d152162ea990088ca0da5dbb30c0
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2017-07-21 21:17:05 +0100

    Started to work on the application structure.

---
+++ schemas/files.sql
@@ -0,0 +1,24 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `filesize` int(11) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+COMMIT;
